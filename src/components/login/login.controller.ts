ed4c7c8df1fa6cea97221b2c582d570e09a9c585
HaiNguyen <hai.nguyen@waverleysoftware.com>
2022-08-19 20:21:27 +0700

    update redirect

--- src/components/login/login.controller.ts
+++ src/components/login/login.controller.ts
@@ -13,7 +13,7 @@ export let login = async (req: Request, res: Response) => {
   try {
     const response = await queryDB("SELECT * from users where username = $1 limit 1;", [req.body.username]);
     if (response.rows.length > 0 && bcrypt.compareSync(req.body.password, response.rows[0].password)) {
-      return res.redirect("/");
+      return res.redirect("/home");
     } else {
       res.render("login/index", {
         title: "Login",
